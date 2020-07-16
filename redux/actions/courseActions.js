f45c8dde5fec4aa2f6ae68403f60a8575416e1b4
Neha Jinwala <njinwala@stevens.edu>
2020-07-16 12:15:59 -0400

    Add files via upload

---
+++ redux/actions/courseActions.js
@@ -0,0 +1,45 @@
+import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
+import { beginApiCall } from "../actions/apiStatusActions";
+
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+}
+
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    dispatch(beginApiCall());
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
